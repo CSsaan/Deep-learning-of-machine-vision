4c1ec1644b8e62cd6d0631dcdc8d88fd2e5eaa27
CSsaan <57102493+CSsaan@users.noreply.github.com>
2021-09-07 15:52:09 +0800

    ch5
    神经网络与误差反向传播算法

---
+++ ch5.神经网络与误差反向传播算法/xor_tf.py
@@ -0,0 +1,65 @@
+# Lab 9 XOR
+import tensorflow as tf
+import numpy as np
+
+tf.set_random_seed(777)  # for reproducibility
+learning_rate = 0.1
+
+x_data = [[0, 0],
+          [0, 1],
+          [1, 0],
+          [1, 1]]
+y_data = [[0],
+          [1],
+          [1],
+          [0]]
+x_data = np.array(x_data, dtype=np.float32)
+y_data = np.array(y_data, dtype=np.float32)
+
+X = tf.placeholder(tf.float32, [None, 2])
+Y = tf.placeholder(tf.float32, [None, 1])
+
+W = tf.Variable(tf.random_normal([2, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+
+# cost/loss function
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
+                       tf.log(1 - hypothesis))
+
+train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Accuracy computation
+# True if hypothesis>0.5 else False
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+# Launch graph
+with tf.Session() as sess:
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        sess.run(train, feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            print(step, sess.run(cost, feed_dict={
+                  X: x_data, Y: y_data}), sess.run(W))
+
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy],
+                       feed_dict={X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
+
+'''
+Hypothesis:  [[ 0.5]
+ [ 0.5]
+ [ 0.5]
+ [ 0.5]]
+Correct:  [[ 0.]
+ [ 0.]
+ [ 0.]
+ [ 0.]]
+Accuracy:  0.5
+'''
